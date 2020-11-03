6b8651139b78658b055e9b1f5146b7e1e97ef1ca
Saturnin <saturnin@MacBook-Air-de-Saturnin-2.local>
2020-11-03 09:41:40 +0100

    without ssim

--- evaluation.py
+++ evaluation.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 
 from benchmark.PSNR import meanPSNR
-from benchmark.SSIM import meanSSIM
+#from benchmark.SSIM import meanSSIM
 from benchmark.speed import speedMetric
 
 def evaluation(in_path, out_path, verbose):
@@ -30,9 +30,9 @@ def evaluation(in_path, out_path, verbose):
     if verbose:
         print("Computing PSNR (YCbCr channels)", flush=True)
     mean_PSNRYCbCr, _ = meanPSNR(label_path, in_path, color_mode='YCbCr', verbose=verbose)
-    if verbose:
-        print("Computing SSIM", flush=True)
-    mean_SSIM, n3 = meanSSIM(label_path, in_path, verbose=verbose)
+    #if verbose:
+       # print("Computing SSIM", flush=True)
+    #mean_SSIM, n3 = meanSSIM(label_path, in_path, verbose=verbose)
 
     benchmark_file = out_path + "Benchmark" + str(begin_time).replace(':', '-') + ".txt"
     with open(os.path.abspath(benchmark_file),'w') as file:
@@ -42,7 +42,7 @@ def evaluation(in_path, out_path, verbose):
         file.write('Mean PSNR(YCbCr): ' + str(mean_PSNRYCbCr) + ' dB' + os.linesep)
         file.write('Mean PSNR(RGB): ' + str(mean_PSNRRGB) + ' dB' + os.linesep)
         file.write('Number of images used for SSIM: ' + str(n3) + os.linesep)
-        file.write('Mean SSIM(Y): ' + str(mean_SSIM) + os.linesep)
+        #file.write('Mean SSIM(Y): ' + str(mean_SSIM) + os.linesep)
 
     if verbose:
         print("Benchmark saved to: " + benchmark_file, flush=True)
