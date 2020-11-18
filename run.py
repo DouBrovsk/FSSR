1fb78e23c0b1fe030f812ccf27a5ac4004fc4126
Saturnin <saturnin@MacBook-Air-de-Saturnin-2.local>
2020-11-18 10:37:32 +0100

    num_shot

--- run.py
+++ run.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     
 
     if opt.mode == 'meta_train':
-        meta_train(logger_name=opt.logger_name,train_path=opt.train_folder, valid_path=opt.valid_folder, batch_size=opt.batch_size, epoch_nb=opt.epoch_nb, learning_rate=opt.learning_rate, meta_learning_rate=opt.meta_learning_rate, save_path=opt.save_weights, verbose=opt.verbose, weights_load=opt.load_weights, loss_func=opt.loss, loss_network=opt.loss_network, network=opt.network_name,num_shot=opt.num_shot)
+        meta_train(logger_name=opt.logger_name,train_path=opt.train_folder, valid_path=opt.valid_folder, batch_size=opt.batch_size,num_shot=opt.num_shot, epoch_nb=opt.epoch_nb, learning_rate=opt.learning_rate, meta_learning_rate=opt.meta_learning_rate, save_path=opt.save_weights, verbose=opt.verbose, weights_load=opt.load_weights, loss_func=opt.loss, loss_network=opt.loss_network, network=opt.network_name)
     elif opt.mode == 'finetune_maml':
         finetuneMaml(train_path=opt.train_folder, valid_path=opt.valid_folder, batch_size=opt.batch_size, epoch_nb=opt.epoch_nb, learning_rate=opt.learning_rate, meta_learning_rate=opt.meta_learning_rate, load_weights=opt.load_weights, save_weights=opt.save_weights, finetune_depth=opt.finetune_depth, network=opt.network_name)
     elif opt.mode == 'meta_upscale':
